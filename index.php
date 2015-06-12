6433a216819dec5c700a333e5c501fb07b127b56
Jorge Chavez <jorge@xpressa.tv>
2015-06-12 16:53:34 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Conquistando Sonrisas</title>
+    <?php include 'includes/head.php'; ?>
+  </head>
+  <body>
+    <?php include 'includes/header.php'; ?>
+    <?php include 'includes/footer.php'; ?>
+  </body>
+</html>
