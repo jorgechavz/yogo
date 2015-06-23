8e5498e73efb211f352d81c6d154dd4dfcea2a10
Jorge Chavez <jorge@xpressa.tv>
2015-06-23 11:29:13 -0600

    meta device-with=with

--- includes/partials/head.php
+++ includes/partials/head.php
@@ -1,10 +1,11 @@
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" href="public/images/favicon.png" type="image/png" />
 <meta name="author" content="Jorge Chavez">
 <meta name="viewport" content="width=device-width, user-scalable=no">
-<link rel="stylesheet" href="public/css/dist/styles.min.css">
-<link rel="stylesheet" href="public/css/dist/normalize.min.css">
-<link rel="stylesheet" href="public/css/dist/foundation.min.css">
+<link rel="stylesheet" href="public/css/styles.css">
+<link rel="stylesheet" href="public/css/normalize.css">
+<link rel="stylesheet" href="public/css/foundation.css">
 <!--[if IE]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
