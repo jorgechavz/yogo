afad7f47d9c72e97be03171f782cdeb42e029a59
Jorge Chavez <jorge@xpressa.tv>
2015-10-13 14:01:33 -0600

    new things

--- includes/partials/head.php
+++ includes/partials/head.php
@@ -1,11 +1,13 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="apple-touch-icon" href="public/images/apple-touch-icon.png">
 <link rel="icon" href="public/images/favicon.png" type="image/png" />
 <meta name="author" content="Jorge Chavez">
 <meta name="viewport" content="width=device-width, user-scalable=no">
 <link rel="stylesheet" href="public/css/styles.css">
+
 <link rel="stylesheet" href="public/css/normalize.css">
 <link rel="stylesheet" href="public/css/foundation.css">
-<!--[if IE]>
-  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<!--[if lt IE 10]>
+  <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
