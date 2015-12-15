47489a16685fb1d302a2d525155e5fdcf4a302a3
Jorge Enrique Chavez Palma <jorge_clan8@hotmail.com>
2015-12-15 12:40:31 -0700

    Delete apple icon and repeated meta tag

--- includes/partials/head.php
+++ includes/partials/head.php
@@ -1,11 +1,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="apple-touch-icon" href="public/images/apple-touch-icon.png">
 <link rel="icon" href="public/images/favicon.png" type="image/png" />
 <meta name="author" content="Jorge Chavez">
-<meta name="viewport" content="width=device-width, user-scalable=no">
 <link rel="stylesheet" href="public/css/styles.css">
-
 <link rel="stylesheet" href="public/css/normalize.css">
 <link rel="stylesheet" href="public/css/foundation.css">
 <!--[if lt IE 10]>
